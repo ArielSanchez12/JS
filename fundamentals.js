c0fb38596702821e031018508c399d0dde840c0b
ArielSanchez12 <riveraariel433@gmail.com>
2025-04-22 15:47:07 -0500

    TestGitHub en VSC

---
+++ fundamentals.js
@@ -0,0 +1,18 @@
+//Variables - Contenedor de informacion
+//let - const, no usaremos var
+let numOrden = 127 //int
+let modulo = "Mis ordenes" //string
+let status = True //bool
+let ordenes = ["Balon", "Camiseta", "Zapatos"] //array - objeto
+let tipoPago = 'E' //char
+let precioFinal = 155.99 //float
+const CI = 1171765322 //int - inmutable por const
+
+//Tipos de datos compuestos
+let detalleOrden = {
+    id: 1,
+    estado: "Activo",
+    items: ["Balon", "Camiseta", "Zapatos"],
+    precio: 155.99
+}
+
